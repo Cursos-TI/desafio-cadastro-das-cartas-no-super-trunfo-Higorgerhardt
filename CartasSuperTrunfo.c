84bac7bd2f4032ee8182e1b8c7c1581882772b6e
Higorgerhardt <shigor814@gmail.com>
2025-08-21 15:06:04 +0000

    adicionado a estrutura if e else para comparar as cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-
+//aqui foi atribuido as variaveis da carta 01 
 int main (){ 
     char Estado1[15];
     char codigo1[15];
     char cidade1[14]; 
-    float pop1 = 0;
-    float area1 = 0;
-    float pib1 = 0;
+    float pop1 = 0.0;
+    float area1 = 0.0;
+    float pib1 = 0.0;
     int turismo1 = 0;
-
+//aqui foi atribuido as variaveis da carta 02
     char Estado2[15];
     char codigo2[15];
     char cidade2[14]; 
-    float pop2 = 0;
-    float area2 = 0;
-    float pib2 = 0;
+    float pop2 = 0.0;
+    float area2 = 0.0;
+    float pib2 = 0.0;
     int turismo2 = 0;
-
+//aqui foi feito a interação com o usuário para coletar os dados da carta 01
     printf("====Escreva os atributos da carta 01:====\n");
     printf("Nome do estado:\n");
     scanf("%14s", Estado1);
@@ -27,14 +27,18 @@ int main (){
     printf("Nome da cidade:\n");
     scanf("%13s", cidade1);
     printf("Quantidade da populacao:\n");
-    scanf("%lf", &pop1);
+    scanf("%3f", &pop1);
+    fflush(stdin);
     printf("Tamanho da area em km²:\n");
-    scanf("%lf", &area1);
+    scanf("%3f", &area1);
+    fflush(stdin);
     printf("PIB da cidade:\n");
-    scanf("%lf", &pib1);
+    scanf("%3f", &pib1);
+    fflush(stdin);
     printf("Pontos turisticos:\n");
     scanf("%d", &turismo1);
-
+    fflush(stdin);
+//aqui foi feito a interação com o usuário para coletar os dados da carta 02
     printf("====Escreva os atributos da carta 02:====\n");
     printf("Nome do estado:\n");
     scanf("%14s", Estado2);
@@ -43,29 +47,48 @@ int main (){
     printf("Nome da cidade:\n");
     scanf("%13s", cidade2);
     printf("Quantidade da populacao:\n");
-    scanf("%f", &pop2);
+    scanf("%3f", &pop2);
+    fflush(stdin);
     printf("Tamanho da area em km²:\n");
-    scanf("%f", &area2);
+    scanf("%3f", &area2);
+    fflush(stdin);
     printf("PIB da cidade:\n");
-    scanf("%f", &pib2);
+    scanf("%3f", &pib2);
+    fflush(stdin);
     printf("Pontos turisticos:\n");
     scanf("%d", &turismo2);
-
+    fflush(stdin);
+//aqui foi feito a exibição dos dados coletados da carta 01
     printf("======Dados da carta 01:======\n");
     printf("===Estado:===\n%s\n", Estado1);
     printf("===Codigo:===\n%s\n", codigo1);
     printf("===Cidade:===\n%s\n", cidade1);
-    printf("===Populacao:===\n%.2f\n", pop1);
-    printf("===Area em km²:===\n%.2f\n", area1);
-    printf("===PIB:===\n%.2f\n", pib1);
+    printf("===Populacao:===\n%3.f\n", pop1);
+    printf("===Area em km²:===\n%3.f\n", area1);
+    printf("===PIB:===\n%3.f\n", pib1);
     printf("===Pontos turisticos:===\n%d\n", turismo1);
-
+//aqui foi feito a exibição dos dados coletados da carta 02
     printf("=====Dados da carta 02:=====\n");
     printf("===Estado:===\n%s\n", Estado2);
     printf("===Codigo:===\n%s\n", codigo2);
     printf("===Cidade:===\n%s\n", cidade2);
-    printf("===Populacao:===\n%.2f\n", pop2);
-    printf("===Area em km²:===\n%.2f\n", area2);
-    printf("===PIB:===\n%.2f\n", pib2);
+    printf("===Populacao:===\n%3.f\n", pop2);
+    printf("===Area em km²:===\n%3.f\n", area2);
+    printf("===PIB:===\n%3.f\n", pib2);
     printf("===Pontos turisticos:===\n%d\n", turismo2);
+//aqui foi feito a comparação dos dados das cartas
+     if (pop1 < pop2) {
+
+        printf("A carta 01 venceu pois tem a menor populacao.\n");
+    } else {
+        printf("A carta 02 venceu pois tem a menor populacao.\n");
+    }
+    if (turismo1 > turismo2)
+    {
+        printf("A carta 01 venceu.\n");
+    }
+    else
+    {
+        printf("A carta 02 venceu.\n");
+    }
 }
